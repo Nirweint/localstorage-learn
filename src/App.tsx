95b2f9aceb0621304174c95899560633ce3994ac
Nirweint <nirweint_1@mail.ru>
2021-11-12 23:00:19 +0300

    localstorage all

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [value, setValue] = useState<number>(0)
+
+    const incHandler = () => {
+        setValue(value + 1)
+    }
+
+    const setToLocalstorageHandler = () => {
+        localStorage.setItem('counterValue', JSON.stringify(value))
+        localStorage.setItem('counterValue + 1', JSON.stringify(value + 1))
+    }
+
+    const getFromLocalstorageHandler = () => {
+        let valueAsString = localStorage.getItem('counterValue')
+       if (valueAsString) {
+           let newValue =JSON.parse(valueAsString)
+            setValue(newValue)
+       }
+    }
+
+    const clearLocalstorageHandler = () => {
+        localStorage.clear()
+        setValue(0)
+    }
+
+    const removeFromLocalstorageHandler = () => {
+        localStorage.removeItem('counterValue + 1')
+    }
+
+    return (
+        <div className="App">
+            <h1>Localstorage</h1>
+            <h1>{value}</h1>
+            <button onClick={incHandler}>inc</button>
+            <button onClick={setToLocalstorageHandler}>setToLocalstorage</button>
+            <button onClick={getFromLocalstorageHandler}>getFromLocalstorage</button>
+            <button onClick={clearLocalstorageHandler}>clearLocalstorage</button>
+            <button onClick={removeFromLocalstorageHandler}>removeFromLocalstorage</button>
+        </div>
+    );
 }
 
 export default App;
